43021f7fabea80fd2841fd84e8fd4ec69c9068e3
Gabz <gabriel.antiqueira8@gmail.com>
2025-04-22 10:16:00 -0300

    feat:newProject

---
+++ src/main/java/org/elink/renov/RenovApplication.java
@@ -0,0 +1,13 @@
+package org.elink.renov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/elink/renov/RenovApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elink.renov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
